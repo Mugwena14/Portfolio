abf48f70edf63036140046f8eac75092c4577a50
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-19 14:37:32 +0200

    Themes

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="min-h-screen">
+        {/* Theme Toggle */}
+
+
+        {/* Background Effects */}
+
+
+        {/* Navbar */}
+
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+        </div>
+    )
+}
+
+export default Home
